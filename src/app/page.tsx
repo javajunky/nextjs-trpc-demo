feb0c52e6c3247760f38c5f5248ec73edf9cad34
Wil <wilchow@gmail.com>
2023-11-08 16:25:10 -0600

    Added tRPC files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import Image from "next/image";
+"use client";
+import { trpc } from "@/utils/trpc";
 
 export default function Home() {
+  const hello = trpc.hello.useQuery({ text: "client" });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-extrabold">Next.js + tRPC = 😎</h1>
-      <section></section>
+      <section className="mt-10 text-xl text-[#bada55]">
+        {!hello.data ? <p>Loading…</p> : <p>{hello.data.greeting}</p>}
+      </section>
     </main>
   );
 }
